5e319852ab85921b568be3555b2e8a6029c8694c
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-03 11:48:12 -0300

    feat: implementa funcionalidade do botão de deletar personagem

--- front/src/component/display/Display.jsx
+++ front/src/component/display/Display.jsx
@@ -1,21 +1,33 @@
 import { useContext } from 'react';
 import './Display.css';
 import HPContext from '../../context/HPContext';
+import axios from 'axios';
 
 function Display() {
-  const { loading, characters } = useContext(HPContext);
+  const { loading, characters, setUpdateList } = useContext(HPContext);
+
+  const handleEdit = (id) => {
+    console.log(id)
+  };
+  
+  const handleDelete = async (id) => {
+    console.log(id);
+    const URL = `http://localhost:3001/character/${id}`;
+    await axios.delete(URL);
+    setUpdateList(true);
+  };
 
   return (
     <section className="display">
       {loading && (<p>Loading</p>)}
-      {!loading && characters.map((char, index) => (
-        <div key={ index } className="character-display">
+      {!loading && characters.map((char) => (
+        <div key={ char.id } className="character-display">
           <h3>{char.character}</h3>
           <aside>
-            <button type="button" onClick={ () => {} }>
+            <button type="button" onClick={ () => handleEdit(char.id) }>
               Edit
             </button>
-            <button type="button" onClick={ () => {} }>
+            <button type="button" onClick={ () => handleDelete(char.id) }>
               Delete
             </button>
           </aside>
