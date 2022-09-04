d8cf6fdc684ba0aed941df7ad40aecedd44b937b
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-03 23:32:07 -0300

    feat: adiciona casas de hogwarts

--- front/src/component/display/Display.jsx
+++ front/src/component/display/Display.jsx
@@ -1,6 +1,10 @@
 import { useContext } from 'react';
 import axios from 'axios';
 import HPContext from '../../context/HPContext';
+import Gryffindor from '../../image/houses/Gryffindor.png';
+import Hufflepuff from '../../image/houses/Hufflepuff.png';
+import Ravenclaw from '../../image/houses/Ravenclaw.png';
+import Slytherin from '../../image/houses/Slytherin.png';
 import './Display.css';
 
 function Display() {
@@ -28,6 +32,11 @@ function Display() {
       {loading && (<p>Loading</p>)}
       {!loading && characters.map((char) => (
         <div key={ char.id } className="character-display">
+          {char.house === 'Gryffindor' && <img src={Gryffindor} alt="Gryffindor logo" className='house-logo' />}
+          {char.house === 'Hufflepuff' && <img src={Hufflepuff} alt="Hufflepuff logo" className='house-logo' />}
+          {char.house === 'Ravenclaw' && <img src={Ravenclaw} alt="Ravenclaw logo" className='house-logo' />}
+          {char.house === 'Slytherin' && <img src={Slytherin} alt="Slytherin logo" className='house-logo' />}
+          {char.house === 'Unknown' && <img src={Slytherin} alt="Slytherin logo" className='unknown' />}
           <h3>{char.character}</h3>
           <aside>
             <button type="button" className="edit-button" onClick={ () => handleEdit(char) }>
