ee9f2967628266493f374f6ab96b9acb69f35096
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-11 22:16:55 -0300

    feat: criação dos arquivos para criação dos testes e correção do linter para essa aplicação

---
+++ back/tests/unit/controllers/CharacterController.test.js
@@ -0,0 +1,22 @@
+// const sinon = require('sinon');
+// const { expect } = require('chai');
+
+describe('Testes no SalesService', () => {
+  describe('1. Insere nova venda no Banco de Dados Sales', () => {
+    describe('caso de falha', () => {
+      before(async () => {});
+
+      after(async () => {});
+
+      it('', async () => {});
+    });
+
+    describe('caso de sucesso', () => {
+      before(async () => {});
+
+      after(async () => {});
+
+      it('', async () => {});
+    });
+  });
+});
